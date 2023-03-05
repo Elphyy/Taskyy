0310985844ced163a24a59ea000868799ec720a9
Lucas Signorelli <47946746+Luquor@users.noreply.github.com>
2023-03-05 18:18:02 +0100

    Connection to local db and rendering requests

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,5 @@ from app import database as data
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", name=data.randomize_name())
+    connection = data.db_connection("localhost", "root", "elphyy")
+    return render_template("index.html", todos=data.get_todos(connection))
