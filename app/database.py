b840f93818d945a2f7270d3b10061389bf5ecbb7
Lucas Signorelli <47946746+Luquor@users.noreply.github.com>
2023-03-05 18:17:04 +0100

    Connection and data fetching from a local database

--- app/database.py
+++ app/database.py
@@ -1,5 +1,21 @@
-import random
+import mysql.connector
 
 
-def randomize_name():
-    return random.choice(["Alice", "Bernard", "Lucas", "Guillaume"])
+def db_connection(host, user, password):
+    database = mysql.connector.connect(host=host, user=user, password=password, database="taskyy")
+    return database
+
+
+def get_todos(database):
+    cursor = database.cursor()
+    cursor.execute('SELECT * FROM Todo')
+    todo_id, name, status = 0, '', ''
+
+    records = cursor.fetchall()
+    print('Total number of rows in table: ', cursor.rowcount)
+    for row in records:
+        todo_id = row[0]
+        name = row[1]
+        status = row[2]
+    return todo_id, name, status
+
